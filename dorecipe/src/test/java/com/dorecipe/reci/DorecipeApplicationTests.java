515fa1cb2e4de8f762e9287770c3bc97b839356f
hirin <janghr0123@gmail.com>
2022-08-11 10:19:10 +0900

    first commit

---
+++ dorecipe/src/test/java/com/dorecipe/reci/DorecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorecipe.reci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
