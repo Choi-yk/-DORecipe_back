8fa1a7fe3e0ae4c73a0b0a3b2639d19a697df430
choiyk <weareyk@gmail.com>
2022-08-11 11:48:37 +0900

    add recipe entity

---
+++ dorecipe/src/main/java/com/dorecipe/main/entity/Recipe.java
@@ -0,0 +1,61 @@
+package com.dorecipe.main.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="recipe")
+public class Recipe {
+	
+	// 레시피
+	int recipe_num;
+	
+	String recipe_title;
+	
+	int recipe_savetype;
+	
+	String recipe_introduce;
+	
+	String recipe_url;
+	
+	String recipe_rpath;
+	
+	String recipe_tag;
+	
+	String category_kind;
+	
+	String category_theme;
+	
+	String category_way;
+	
+	String category_ing;
+
+	 // 요리 정보
+	String information_person;
+	
+	String infotmaion_time;
+	
+	String infotmaion_level;
+	
+	// 요리 완성
+	String completion_path1;
+	
+	String completion_path2;
+	
+	String completion_path3;
+	
+	String completion_path4;
+	
+	String completion_tip;
+	
+	LocalDateTime recipe_creDate;
+	
+	String member_id;
+}
