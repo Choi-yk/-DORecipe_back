df00da77a657ffa820b417434f1d04cf20c01a4e
ksytulip <tulip_22@naver.com>
2022-08-11 15:49:47 +0900

    add SecurityConfig

--- dorecipe/src/main/java/com/dorecipe/main/SecurityConfig.java
+++ dorecipe/src/main/java/com/dorecipe/main/SecurityConfig.java
@@ -11,8 +11,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll()
-                ;
+        http.authorizeRequests().antMatchers("/**").permitAll();
+                
         return http.build();
     }
 }
+
