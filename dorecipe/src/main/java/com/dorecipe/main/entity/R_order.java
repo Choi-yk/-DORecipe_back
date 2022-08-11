6e298f09f90c862985926732e1ade5610ef19f3a
choiyk <weareyk@gmail.com>
2022-08-11 17:22:28 +0900

    choiyk: annotion T.T

--- dorecipe/src/main/java/com/dorecipe/main/entity/R_order.java
+++ dorecipe/src/main/java/com/dorecipe/main/entity/R_order.java
@@ -1,31 +1,41 @@
 package com.dorecipe.main.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data // equals, hashCode 구현 (Lombok의 @Data 어노테이션 추가로 자동 생성)
 @Getter
 @Setter
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor // 디폴트 생성자가 존재 (Lombok의 @NoArgsConstructor 어노테이션 추가로 자동 생성)
 @AllArgsConstructor
 @IdClass(Order_Id.class) // 복합키 사용
 @Table(name="r_order")
-public class R_order {
+public class R_order implements Serializable {
 	
 	@Id
-	int recipe_num;
+	@JoinColumn(name = "recipe_num")
+	private String recipe_num;
+	//private Recipe recipe;
 	
 	@Id
 	int order_num;
 	
+	@Column(length = 200)
 	String order_explain;
 	
+	@Column(length = 100)
 	String order_path;
 }
--- dorecipe/src/main/java/com/dorecipe/main/entity/Recipe.java
+++ dorecipe/src/main/java/com/dorecipe/main/entity/Recipe.java
@@ -2,11 +2,16 @@ package com.dorecipe.main.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.CreatedDate;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -22,48 +27,66 @@ public class Recipe {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int recipe_num;
 	
+	@NotBlank(message = "제목을 입력해 주세요.") // null, "", " " 불가능
+	@Column(length = 100)
 	private String recipe_title;
 	
 	private int recipe_savetype;
 	
+	@Column(length = 200)
 	private String recipe_introduce;
 	
+	@Column(length = 40)
 	private String recipe_url;
 	
+	@Column(length = 40)
 	private String recipe_rpath;
 	
+	@Column(length = 100)
 	private String recipe_tag;
 	
 	// 카테고리
+	@Column(length = 20)
 	private String category_kind;
 	
+	@Column(length = 20)
 	private String category_theme;
 	
+	@Column(length = 20)
 	private String category_way;
 	
+	@Column(length = 200)
 	private String category_ing;
 
 	 // 요리 정보
+	@Column(length = 10)
 	private String information_person;
 	
+	@Column(length = 10)
 	private String infotmaion_time;
 	
+	@Column(length = 10)
 	private String infotmaion_level;
 	
 	// 요리 완성
+	@Column(length = 100)
 	private String completion_path1;
 	
+	@Column(length = 100)
 	private String completion_path2;
 	
+	@Column(length = 100)
 	private String completion_path3;
 	
+	@Column(length = 100)
 	private String completion_path4;
 	
+	@Column(length = 100)
 	private String completion_tip;
 	
+	@CreatedDate
 	private LocalDateTime recipe_creDate;
 	
-	
-	
-	String member_id;
+	@JoinColumn(name = "member_id")
+	private Member member;
 }
